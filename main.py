df6f9532da79b0bff4834bac5c407f4747d5d70b
justkeepgoin <manage06@naver.com>
2023-07-06 22:19:38 +0900

    로그인 인증 기능 추가
    로그인 인증 기능을 추가했습니다

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from fastapi import FastAPI, UploadFile, Form, Response
+from fastapi import FastAPI, UploadFile, Form, Response, Depends
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
 from fastapi.staticfiles import StaticFiles
+from fastapi_login import LoginManager
+from fastapi_login.exceptions import InvalidCredentialsException
 from typing import Annotated
 import sqlite3
 
@@ -23,6 +25,58 @@ cur.execute(
 
 app = FastAPI()
 
+SERCRET = "jjang-gu"
+manager = LoginManager(SERCRET, "/login")
+
+
+@manager.user_loader()
+def query_user(data):
+    WHERE_STATEMENTS = f'id="{data}"'
+    if type(data) == dict:
+        WHERE_STATEMENTS = f'''id="{data["id"]}"'''
+
+    con.row_factory = sqlite3.Row
+    cur = con.cursor()
+    user = cur.execute(
+        f"""
+                       SELECT * from users WHERE {WHERE_STATEMENTS}
+                       """
+    ).fetchone()
+    return user
+
+
+@app.post("/login")
+def login(id: Annotated[str, Form()], password: Annotated[str, Form()]):
+    user = query_user(id)
+    print(user["password"])
+    if not user:
+        raise InvalidCredentialsException
+    elif password != user["password"]:
+        raise InvalidCredentialsException
+
+    access_token = manager.create_access_token(
+        data={"sub": {"id": user["id"], "name": user["name"], "email": user["email"]}}
+    )
+
+    return {"access_token": access_token}
+
+
+@app.post("/signup")
+def signup(
+    id: Annotated[str, Form()],
+    password: Annotated[str, Form()],
+    name: Annotated[str, Form()],
+    email: Annotated[str, Form()],
+):
+    cur.execute(
+        f"""
+                INSERT INTO users(id,name,email,password)
+                VALUES ('{id}','{name}','{email}','{password}')
+                """
+    )
+    con.commit()
+    return "200"
+
 
 @app.post("/items")
 async def create_item(
@@ -32,6 +86,7 @@ async def create_item(
     description: Annotated[str, Form()],
     place: Annotated[str, Form()],
     insertAt: Annotated[int, Form()],
+    user=Depends(manager),
 ):
     image_bytes = await image.read()
     cur.execute(
@@ -45,7 +100,7 @@ async def create_item(
 
 
 @app.get("/items")
-async def get_items():
+async def get_items(user=Depends(manager)):
     con.row_factory = sqlite3.Row
     cur = con.cursor()
     rows = cur.execute(
@@ -67,23 +122,6 @@ async def get_image(item_id):
     ).fetchone()[0]
 
     return Response(content=bytes.fromhex(image_bytes))
-
-
-@app.post("/signup")
-def signup(
-    id: Annotated[str, Form()],
-    password: Annotated[str, Form()],
-    name: Annotated[str, Form()],
-    email: Annotated[str, Form()],
-):
-    cur.execute(
-        f"""
-                INSERT INTO users(id,name,email,password)
-                VALUES ('{id}','{name}','{email}','{password}')
-                """
-    )
-    con.commit()
-    return "200"
 
 
 app.mount("/", StaticFiles(directory="frontend", html=True), name="fromtend")
